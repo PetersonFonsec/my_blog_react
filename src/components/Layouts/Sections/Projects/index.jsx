323355a13e9a68e3da947967dc61cccbf99c9f3d
Peterson <petersonsimiao1@gmail.com>
2021-02-07 00:42:28 -0300

    feat: Montado sessão de projetos

---
+++ src/components/Layouts/Sections/Projects/index.jsx
@@ -0,0 +1,20 @@
+import FormSearch from "../../../Forms/Search";
+import ListProjects from "../../../Lists/Projects";
+
+import { Section, Wrapper, Title } from "./style";
+
+function SectionProjects() {
+  return (
+    <>
+      <Section>
+        <Wrapper>
+          <Title>My Projects</Title>
+          <FormSearch />
+          <ListProjects />
+        </Wrapper>
+      </Section>
+    </>
+  );
+}
+
+export default SectionProjects;
